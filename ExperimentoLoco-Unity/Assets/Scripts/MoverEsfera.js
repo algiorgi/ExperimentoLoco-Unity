f84944702dd1d65a0f5b2a10631ad6fff0102406
geagiorgi <alejandro.giorgi@grupoesfera.com.ar>
2015-12-12 10:15:03 -0300

    Se agrega script en js para desplazar la esfera

---
+++ ExperimentoLoco-Unity/Assets/Scripts/MoverEsfera.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+var velocidadDeDesplazamiento : float = 2;
+
+function Start () {
+
+}
+
+function Update () {
+
+	transform.Translate(Vector3.down * Time.deltaTime * velocidadDeDesplazamiento);
+}
