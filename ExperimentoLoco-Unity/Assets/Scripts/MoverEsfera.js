4346dee8d0cb157600580365824b8c3c301730f3
geagiorgi <alejandro.giorgi@grupoesfera.com.ar>
2015-12-12 10:19:45 -0300

    Se agrega una condicion para reposicionar la esfera si se sale de la pantalla

--- ExperimentoLoco-Unity/Assets/Scripts/MoverEsfera.js
+++ ExperimentoLoco-Unity/Assets/Scripts/MoverEsfera.js
@@ -1,12 +1,21 @@
 ﻿#pragma strict
 
 var velocidadDeDesplazamiento : float = 2;
+var limiteInferior : int = -6;
+
+var posicionInicial : Vector3;
 
 function Start () {
 
+	posicionInicial = transform.position;
 }
 
 function Update () {
 
 	transform.Translate(Vector3.down * Time.deltaTime * velocidadDeDesplazamiento);
+
+	if (transform.position.y < limiteInferior) {
+
+		transform.position = posicionInicial;
+	}  
 }
